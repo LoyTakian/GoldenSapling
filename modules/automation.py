7679e1c7d8f8ef6a4cd2ce76d4738d264f6a326d
Loy Takian <loytakian@gmail.com>
2023-12-27 20:03:52 +0000

    feat: Auto restart if HUB crashes

--- modules/automation.py
+++ modules/automation.py
@@ -1,13 +1,23 @@
 import os
 import time
+import requests
+from datetime import datetime
 from dotenv import load_dotenv
 from r5rIntegration import read_and_process_txt_file
 from dbIntegration import read_leaderboard, allowed_table_names
 from r5rIntegration import update_top3_file
 
 load_dotenv()
+R5R_SERVER_EXE = os.getenv('R5R_SERVER_EXE')
+R5R_SERVERS_URL = os.getenv('R5R_SERVERS_URL')
 RECORDS_FOLDER_PATH = os.getenv('RECORDS_FOLDER_PATH')
 
+def is_hub_running(server_list, server_name):
+    for server in server_list:
+        if server.get("name") == server_name:
+            return True
+    
+    return False
 
 while True:
     files = [f for f in os.listdir(RECORDS_FOLDER_PATH) if f.endswith('.txt')]
@@ -25,5 +35,20 @@ while True:
         entries[table] = leaderboard
 
     update_top3_file(entries)
+
+    response = requests.post(R5R_SERVERS_URL)
+
+    json_response = response.json()
+
+    if not is_hub_running(json_response.get("servers"), "[NA] MOVEMENT HUB - Test Release"):
+        os.system("taskkill /f /im r5apex_ds.exe")
+        try:
+            current_time = datetime.now()
+            formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+            print(f'Restarting HUB - {formatted_time}')
+            os.chdir(os.path.dirname(R5R_SERVER_EXE))
+            os.startfile(R5R_SERVER_EXE)
+        except FileNotFoundError:
+            print('File not found! Check the file path.')
 
     time.sleep(60)
